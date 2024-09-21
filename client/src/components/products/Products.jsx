511f50a100efee90d3113b9353afb674bd75787f
Web-Mastermind <azeagent@gmail.com>
2024-09-21 22:04:52 +0400

    product page WIP

--- client/src/components/products/Products.jsx
+++ client/src/components/products/Products.jsx
@@ -2,7 +2,71 @@ import React from 'react'
 
 const Products = () => {
   return (
-    <main>Products</main>
+    <main className="product-wrapper grid grid-cols-card gap-4">
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-image">
+          <img className="h-28 object-cover w-full border-b" src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg" alt="" />
+        </div>
+        <div className="product-info p-6">
+          <p className="font-bold">Apple</p>
+          <p>2$</p>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-image">
+          <img className="h-28 object-cover w-full border-b" src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg" alt="" />
+        </div>
+        <div className="product-info p-6">
+          <p className="font-bold">Apple</p>
+          <p>2$</p>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-image">
+          <img className="h-28 object-cover w-full border-b" src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg" alt="" />
+        </div>
+        <div className="product-info p-6">
+          <p className="font-bold">Apple</p>
+          <p>2$</p>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-image">
+          <img className="h-28 object-cover w-full border-b" src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg" alt="" />
+        </div>
+        <div className="product-info p-6">
+          <p className="font-bold">Apple</p>
+          <p>2$</p>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-image">
+          <img className="h-28 object-cover w-full border-b" src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg" alt="" />
+        </div>
+        <div className="product-info p-6">
+          <p className="font-bold">Apple</p>
+          <p>2$</p>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-image">
+          <img className="h-28 object-cover w-full border-b" src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg" alt="" />
+        </div>
+        <div className="product-info p-6">
+          <p className="font-bold">Apple</p>
+          <p>2$</p>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-image">
+          <img className="h-28 object-cover w-full border-b" src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg" alt="" />
+        </div>
+        <div className="product-info p-6">
+          <p className="font-bold">Apple</p>
+          <p>2$</p>
+        </div>
+      </div>
+    </main>
   )
 }
 
