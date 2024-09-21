c2d7acf98e667e223662ccd7b6af407f8e1a87c7
Web-Mastermind <azeagent@gmail.com>
2024-09-21 21:41:37 +0400

    categories component

---
+++ client/src/components/categories/Categories.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <aside>
+      <ul className="flex flex-col gap-4 text-lg">
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+          <p>All</p>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+          <p>Products</p>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+          <p>Drink</p>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+          <p>Drink</p>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+          <p>Drink</p>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+          <p>Drink</p>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+          <p>Drink</p>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+          <p>Drink</p>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+          <p>Drink</p>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+          <p>Drink</p>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+          <p>Drink</p>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+          <p>Drink</p>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+          <p>Drink</p>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+          <p>Drink</p>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+          <p>clen</p>
+        </li>
+      </ul>
+    </aside>
+  )
+}
+
+export default Categories
