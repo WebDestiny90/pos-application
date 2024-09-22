de9278156de3db63e59453d3d7aaf24cd7019cd2
Web-Mastermind <azeagent@gmail.com>
2024-09-22 20:19:13 +0400

    cart items

--- client/src/components/cart/CartTotals.jsx
+++ client/src/components/cart/CartTotals.jsx
@@ -1,13 +1,66 @@
 import { Button } from "antd"
-import { ClearOutlined } from "@ant-design/icons"
+import { ClearOutlined, PlusCircleOutlined, MinusCircleOutlined } from "@ant-design/icons"
 
 const CartTotals = () => {
   return (
     <aside className="cart h-full max-h-[calc(100vh_-_90px)] flex flex-col">
       <h2 className="bg-blue-600 text-center py-4 text-white font-bold tracking-wide">Items in cart</h2>
-      <div className="cart-items">
-        <div className="cart-item">cart item</div>
-      </div>
+      <ul className="cart-items px-2 flex flex-col gap-y-3 py-2 overflow-y-auto">
+        <li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img className="w-16 h-16 object-cover pt-2" src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg" alt="" />
+            <div className="flex flex-col ml-2">
+              <strong>Apple</strong>
+              <span>2$ x 3</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-1">
+            <Button className="w-full !rounded-full" type="primary" size="small" icon={<PlusCircleOutlined />}></Button>
+            <span>1</span>
+            <Button className="w-full !rounded-full" type="primary" size="small" icon={<MinusCircleOutlined />}></Button>
+          </div>
+        </li>
+        <li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img className="w-16 h-16 object-cover pt-2" src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg" alt="" />
+            <div className="flex flex-col ml-2">
+              <strong>Apple</strong>
+              <span>2$ x 3</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-1">
+            <Button className="w-full !rounded-full" type="primary" size="small" icon={<PlusCircleOutlined />}></Button>
+            <span>1</span>
+            <Button className="w-full !rounded-full" type="primary" size="small" icon={<MinusCircleOutlined />}></Button>
+          </div>
+        </li><li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img className="w-16 h-16 object-cover pt-2" src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg" alt="" />
+            <div className="flex flex-col ml-2">
+              <strong>Apple</strong>
+              <span>2$ x 3</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-1">
+            <Button className="w-full !rounded-full" type="primary" size="small" icon={<PlusCircleOutlined />}></Button>
+            <span>1</span>
+            <Button className="w-full !rounded-full" type="primary" size="small" icon={<MinusCircleOutlined />}></Button>
+          </div>
+        </li><li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img className="w-16 h-16 object-cover pt-2" src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg" alt="" />
+            <div className="flex flex-col ml-2">
+              <strong>Apple</strong>
+              <span>2$ x 3</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-1">
+            <Button className="w-full !rounded-full" type="primary" size="small" icon={<PlusCircleOutlined />}></Button>
+            <span>1</span>
+            <Button className="w-full !rounded-full" type="primary" size="small" icon={<MinusCircleOutlined />}></Button>
+          </div>
+        </li>
+      </ul>
       <div className="cart-totals mt-auto">
         <div className="border-t border-b">
           <div className="flex justify-between p-2">
