dc1d0930dd5e3d370cdeddabf9c4b42c23e1271d
Web-Mastermind <azeagent@gmail.com>
2024-09-25 17:52:58 +0400

    added route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePages from "./pages/HomePages"
 import CartPage from "./pages/CartPage"
+import InvoicesPage from "./pages/InvoicesPage"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePages />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/invoices" element={<InvoicesPage />} />
       </Routes>
     </BrowserRouter>
 
