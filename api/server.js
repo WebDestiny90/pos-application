9e41721081c22f6d344219d866ef49905c81b545
Web-Mastermind <azeagent@gmail.com>
2024-09-29 00:05:08 +0400

    test?!

--- api/server.js
+++ api/server.js
@@ -1,12 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
 const PORT = 5000;
 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("Sucesfully connected");
+
+  }
+  catch (error) {
+    throw error
+  }
+}
+
 app.get("/", (req, res) => res.send("Test"));
 
 app.listen(PORT, () => {
+  connect();
   console.log(`Example app listening on port ${PORT}`);
 
 })
