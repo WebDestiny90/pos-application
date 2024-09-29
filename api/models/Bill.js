241585610cac9e36b34117eedaa153b6f25eb4e1
Web-Mastermind <azeagent@gmail.com>
2024-09-29 22:05:40 +0400

    bill scheme

---
+++ api/models/Bill.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const BillScheme = mongoose.Schema(
+  {
+    customerName: { type: String, require: true },
+    customerPhoneNumber: { type: String, require: true },
+    paymentMode: { type: String, require: true },
+    subTotal: { type: Number, require: true },
+    tax: { type: Number, require: true },
+    totalAmount: { type: Number, require: true },
+    cartItems: { type: Array, require: true },
+  },
+  {
+    timestamps: true
+  }
+)
+
+const Bill = mongoose.model("bills", BillScheme);
+module.exports = Category;
