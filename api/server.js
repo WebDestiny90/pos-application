e36b6f85da986b930db3b1ed1060f8da3385986b
Web-Mastermind <azeagent@gmail.com>
2024-09-30 23:00:46 +0400

    added cors

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
+const categoryRoute = require("./routes/categories.js")
 
 dotenv.config();
 
@@ -11,7 +14,7 @@ const PORT = 5000;
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI)
-    console.log("Succesfully connected");
+    console.log("Successfully connected");
 
   }
   catch (error) {
@@ -19,7 +22,11 @@ const connect = async () => {
   }
 }
 
-app.get("/", (req, res) => res.send("Test"));
+app.use(express.json());
+app.use(cors());
+app.use("/api/categories", categoryRoute);
+
+// app.get("/", (req, res) => res.send("Test"));
 
 app.listen(PORT, () => {
   connect();
